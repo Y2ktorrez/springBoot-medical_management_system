7863f741f5a79cef46bace223beb6f36c712e9b8
Y2ktorrez <andrestorrezvaca@gmail.com>
2024-10-22 14:10:35 -0400

    Creacion del Proyecto

---
+++ src/test/java/com/y2k/medical_management_system/MedicalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2k.medical_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
